ad1f71d854ccd5b3223b9e592cac3dbf13368dc7
lubaochuan <lubaochuan@gmail.com>
2017-10-10 15:16:30 -0500

    added dummy MainScreen

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
-import store from './store';
+import {StackNavigator} from 'react-navigation';
 
-export default class App extends React.Component {
+import store from './store';
+import MainScreen from './screens/MainScreen';
+
+export default class App extends Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Main: {screen: MainScreen}
+    });
+
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/MainScreen.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {View, Text, Platform} from 'react-native';
+import {STATUS_BAR_HEIGHT} from '../constants';
+
+class MainScreen extends Component{
+  static navigationOptions = () => ({
+    title: 'Capo Keys',
+    headerStyle: {
+      height: Platform.OS === 'android'? 54+STATUS_BAR_HEIGHT:54,
+      backgroundColor: '#2196F3'
+    },
+    headerTitleStyle: {
+      marginTop: Platform.OS === 'android'? STATUS_BAR_HEIGHT:0,
+      color: 'white'
+    },
+    headerLeft: <View><Text>I</Text></View>
+  });
+
+  render(){
+    return (
+      <View style={{flex:1, backgroundColor: '#ddd'}}>
+        {/* chord Modal */}
+
+        {/* Content */}
+
+      </View>
+    );
+  }
+
+}
+
+export default MainScreen;
